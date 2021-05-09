4e8c01e21ea3f26b76685067c47ac1efaadf1a9d
Katelyn <katelynxgraham@gmail.com>
2021-05-09 01:09:47 +0100

    Lab movie app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MoviesService} from '../Services/movies.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+MovieData:any = [];
+  constructor(private movieServices:MoviesService) {}
 
-  constructor() {}
-
+  ngOnInit(){
+    this.movieServices.GetMovieData().subscribe(
+      (data)=>{
+          this.MovieData = data.Search;
+          console.log(this.MovieData);
+      }
+    );
+  }
 }
